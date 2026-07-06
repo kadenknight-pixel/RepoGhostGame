4ee3fb9afa83ed831f867f780a36fefa75842493
Noah <noahmorris@live.com>
2026-07-06 15:24:20 +1000

    Progen Mk1
    fuck off

---
+++ Assets/Progen/_Scripts/SimpleRandomWalkDungeonGenerator.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class SimpleRandomWalkDungeonGenerator : MonoBehaviour
+{
+    [SerializeField]
+    protected Vector2Int startPosition = Vector2Int.zero;
+    [SerializeField]
+    private int interations = 10;
+    [SerializeField]
+    public int walklength = 10;
+    [SerializeField]
+    public bool startRandomlyEachInteration = true;
+
+    public void RunProcedualGeneration()
+    {
+        HashSet<Vector2Int> floorPositions = RunRandomWalk();
+        foreach (var position in floorPositions)
+        {
+            Debug.Log(position);
+        };
+    }
+
+    protected HashSet<Vector2Int> RunRandomWalk()
+    {
+        var currentPosition = startPosition;
+        HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
+
+        for (int i = 0; i < interations; i++)
+        {
+            var path = ProcedualGenerationAlgorithms.SimpleRandomWalk(currentPosition, walklength);
+            floorPositions.UnionWith(path);
+            if(startRandomlyEachInteration)
+                currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
+        }
+        return floorPositions;
+    }
+}
